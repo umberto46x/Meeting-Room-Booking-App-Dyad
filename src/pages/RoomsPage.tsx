1c56f3fcc9075f0cc533993aa38e35d891dd5190
[dyad] <daniele.u.nicosia.dev@gmail.com>
2025-11-07 13:34:44 +0100

    [dyad] wrote 1 file(s)

--- src/pages/RoomsPage.tsx
+++ src/pages/RoomsPage.tsx
@@ -4,7 +4,7 @@ import { mockRooms } from "@/data/mockData";
 
 const RoomsPage: React.FC = () => {
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto"> {/* Removed p-4 */}
       <h1 className="text-4xl font-bold text-center mb-8">Sale Riunioni Disponibili</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {mockRooms.map((room) => (
